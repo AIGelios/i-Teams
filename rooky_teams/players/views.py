a8c4810c5261e4ba83b147354dd73f054ac75549
Anton Kadkin <122638842+AIGelios@users.noreply.github.com>
2025-04-22 10:05:41 +0500

    Update views.py

--- rooky_teams/players/views.py
+++ rooky_teams/players/views.py
@@ -119,13 +119,13 @@ class RosterClearView(TemplateView):
 
 class GenerateLineupsView(TemplateView):
     template_name = 'players/lineups.html'
-    teams = {}
 
     def get(self, request, *args, **kwargs):
         success_message = _('Lineups generated successfully.')
         too_few_players_message = _(
             'Unable to create teams. Add more players to the roster!')
-        context = self.__class__.teams = generate_balanced_teams()
+        self.__class__.teams = generate_balanced_teams()
+        context = self.get_context_data() | self.__class__.teams
         if not context['team_1'] or not context['team_2']:
             messages.error(self.request, too_few_players_message)
             return redirect(PLAYERS_INDEX_URL)
@@ -134,7 +134,7 @@ class GenerateLineupsView(TemplateView):
 
     def post(self, request, *args, **kwargs):
         success_message = _('Match successfully created.')
-        context = self.__class__.teams
+        context = self.get_context_data() | self.__class__.teams
         match = create_match(
             get_team_ids_json(context['team_1']),
             get_team_ids_json(context['team_2']),
