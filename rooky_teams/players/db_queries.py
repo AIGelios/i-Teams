57ac6389be517f2f2c0bd3b4b044bbd238b68ff3
Anton Kadkin <122638842+AIGelios@users.noreply.github.com>
2025-06-05 10:36:14 +0500

    Update db_queries.py

--- rooky_teams/players/db_queries.py
+++ rooky_teams/players/db_queries.py
@@ -30,3 +30,8 @@ def get_team_queryset(player_ids_json_string):
 
 def add_player_to_team(id, team_number):
     Player.objects.filter(id=id).update(team=team_number)
+
+def change_team(id):
+    player = Player.objects.filter(id=id)
+    team_number = 1 if player.team in (0, 2) else 2
+    player.update(team=team_number)
