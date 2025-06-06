5324d93dc2f4e20256a88dea736980f89259af16
Anton Kadkin <122638842+AIGelios@users.noreply.github.com>
2025-06-06 10:28:42 +0500

    Update views.py

--- rooky_teams/players/views.py
+++ rooky_teams/players/views.py
@@ -7,8 +7,8 @@ from django.views.generic import (
 from django_filters.views import FilterView
 from .models import Player
 from .db_queries import (
-    add_to_roster, delete_from_roster, clear_roster,
-    add_player_to_team, change_team,
+    add_to_roster, delete_from_roster,
+    clear_roster, change_team,
 )
 from .forms import PlayerForm
 from .filters import PlayerFilterSet
